91d64be1bef29fa8b675fdbb2ef7c18ca9ae7e0e
Eak Netpanya <eak.netpanya@gmail.com>
2025-04-18 22:48:17 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export"
+  reactStrictMode: true,
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
 };
 
 export default nextConfig;
