a2f02ca265d3c90dba46a8d96fe83c4110a7baf5
Eak Netpanya <eak.netpanya@gmail.com>
2025-04-18 23:59:04 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Sidebar } from "./components/Sidebar";
+
 import "./globals.css";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-sm-3" style={{ 'fontSize': 12.5 }}>
+              <Sidebar />
+            </div>
+            <div className="col-sm-9">
+              <div className="container-fluid">{children}</div>
+            </div>
+          </div>
+        </div>
       </body>
     </html>
   );
