6b340cc9fed3411fe78922484ccf5af99a713991
Eak Netpanya <eak.netpanya@gmail.com>
2025-04-19 02:29:16 +0700

    update

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { getAllPosts, getPostSlugs, PostType } from "@/lib/get-posts";
+import { getAllPosts } from "@/lib/get-posts";
 import Link from "next/link";
 
 export const Sidebar = async function () {
@@ -8,7 +8,6 @@ export const Sidebar = async function () {
             <h2>one.ilmsg.in.th</h2>
             <ul className="list-group">
                 <li key={99} className="list-group-item"><Link href="/">⛪ Home</Link></li>
-                {/* <li key={98} className="list-group-item"><Link href="/posts">⛺ Posts</Link></li> */}
                 {posts.map((post) => (
                     <li key={post.id} className="list-group-item">
                         {post.emoji} <span className="badge bg-primary">{post.type}</span>&nbsp;
